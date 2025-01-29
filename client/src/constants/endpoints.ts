42cf5e0176fe059dd843350fd6ad67c79f946d7b
Park-Mu-Seong <63047990+CodeVac513@users.noreply.github.com>
2025-01-29 10:46:32 +0900

    Update endpoints.ts

--- client/src/constants/endpoints.ts
+++ client/src/constants/endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://192.168.0.27";
+export const BASE_URL = "parkmuseong.iptime.org";
 export const CHAT_SERVER_URL = "https://denamu.site";
 export const ADMIN = {
   LOGIN: "/api/admin/login",
